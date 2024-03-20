958582ad9569999e16e1bd4fd6e577086092347f
DineshNagaraj00 <dineshck75@gmail.com>
2024-03-20 16:28:55 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import './App.css';
+
+function App() {
+  const [advice, setAdvice] = useState("");
+
+  useEffect(() => {
+    fetchAdvice();
+  }, []);
+
+  const fetchAdvice = () => {
+    axios.get('https://api.adviceslip.com/advice')
+    .then((response) => {
+      const { advice } = response.data.slip;
+      setAdvice(advice);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  }
+
+  return (
+    <div className="app">
+      <div className="card">
+        <h1 className="heading">{advice}</h1>
+        <button className="button" onClick={fetchAdvice}>
+          <span>Give Me Advice!</span>
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
